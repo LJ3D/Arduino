00f75e50a5625f1e4ca7c63d6c105d184bd44c9e
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-06 20:13:42 +0100

    Update serialMonitor.c

--- serialMonitor.c
+++ serialMonitor.c
@@ -1,10 +1,10 @@
 #include <stdio.h> 
+#include <time.h>
 #include <string.h>
 #include <unistd.h>  
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
-
 
 #define DEVTTY "/dev/ttyACM0"
 
@@ -17,8 +17,8 @@ int open_port(){
 	fcntl(fd, F_SETFL, 0); // Blocking mode - waits for data in input buffer
 	struct termios options; // Port options
 	tcgetattr(fd, &options); // Get the current options for the port
-	cfsetispeed(&options, B57600); // Set baud rates
-	cfsetospeed(&options, B57600);
+	cfsetispeed(&options, B9600); // Set baud rates
+	cfsetospeed(&options, B9600);
 	options.c_cflag |= (CLOCAL | CREAD); // Enable the receiver and set local mode
 	tcsetattr(fd, TCSANOW, &options); // Set the new options for the port
 	options.c_cflag &= ~CSIZE; // Mask the character size bits
@@ -31,12 +31,21 @@ int open_port(){
 	return fd;
 }
 
+void delay(int milliseconds){
+	long pause = milliseconds*(CLOCKS_PER_SEC/1000);
+	clock_t now, then;
+	now = then = clock();
+	while(now-then < pause){
+		now = clock();
+	}
+}
 
 #define BUFFERSIZE 16
 
 int main(){
 	int port = open_port();
 	unsigned char buffer[BUFFERSIZE];
+	delay(2500); // Give arduino a second to start
 	while(1){
 		int n = read(port, buffer, BUFFERSIZE);
 		for(int i=0; i<n; i++){
