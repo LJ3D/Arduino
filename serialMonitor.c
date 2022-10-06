3b6863b7996d739263a09165cb66b8e7c760c132
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-06 20:28:15 +0100

    Update serialMonitor.c

--- serialMonitor.c
+++ serialMonitor.c
@@ -1,19 +1,19 @@
 #include <stdio.h> 
-#include <time.h>
 #include <string.h>
 #include <unistd.h>  
 #include <fcntl.h>
 #include <errno.h>
-#include <termios.h>
+//#include <termios.h>
 
 #define DEVTTY "/dev/ttyACM0"
 
 int open_port(){
 	int fd = open(DEVTTY, O_RDWR | O_NOCTTY | O_NDELAY);
-	if(fd==-1){
+	if(fd == -1){
 		fprintf(stderr, "Failed to open port\n");
-		return 1;
+		return -1;
 	}
+	/*
 	fcntl(fd, F_SETFL, 0); // Blocking mode - waits for data in input buffer
 	struct termios options; // Port options
 	tcgetattr(fd, &options); // Get the current options for the port
@@ -28,24 +28,24 @@ int open_port(){
 	options.c_cflag &= ~CSIZE;
 	options.c_cflag |= CS8; // 8 bits
 	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Raw input mode
+	*/
 	return fd;
 }
 
-void delay(int milliseconds){
-	long pause = milliseconds*(CLOCKS_PER_SEC/1000);
-	clock_t now, then;
-	now = then = clock();
-	while(now-then < pause){
-		now = clock();
-	}
+// Sleep the program for n milliseconds
+void sleep_ms(int milliseconds){
+	usleep(milliseconds * 1000);
 }
 
 #define BUFFERSIZE 16
 
 int main(){
 	int port = open_port();
+	if(port == -1){
+		return -1;
+	}
 	unsigned char buffer[BUFFERSIZE];
-	delay(2500); // Give arduino a second to start
+	sleep_ms(2500); // Give arduino a second to start
 	while(1){
 		int n = read(port, buffer, BUFFERSIZE);
 		for(int i=0; i<n; i++){
