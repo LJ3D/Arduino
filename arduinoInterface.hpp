6731f4d4c616aff84e41fa710938b6ff298ff743
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 11:27:44 +0100

    Basic graphing working! yay!

--- arduinoInterface.hpp
+++ arduinoInterface.hpp
@@ -2,12 +2,14 @@
 /*
 	* Using C-style IO for the /dev/ttyACM(X) interface, even though I have to know what an "pointer" is its easier than dealing with C++ stuff
 	* Low level my beloved
+	* This class is not very good right now, but later i will add a lot of functionality to it (maybe)
 */
 
 #include <stdio.h> 
 #include <unistd.h>  
 #include <fcntl.h>
 #include <errno.h>
+#include <termios.h>
 
 #include <string>
 #include <stdexcept>
@@ -19,44 +21,70 @@
 
 class arduinoPortIO{
 private:
-int fd;
-std::string ttyName;
-unsigned char buffer[BUFFERSIZE];
-char requestByte; // The byte sent to the arduino to request data
+	int fd;
+	std::string ttyName;
+	unsigned char buffer[BUFFERSIZE];
 
-void readIntoBuffer(){
-		int n = read(this->fd, this->buffer, BUFFERSIZE);
+	int open_port(std::string tty){
+		int fd = open(tty.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+		if(fd == -1){
+			fprintf(stderr, "Failed to open port\n");
+			return -1;
+		}
+		fcntl(fd, F_SETFL, 0); // Blocking mode - waits for data in input buffer
+		struct termios options; // Port options
+		tcgetattr(fd, &options); // Get the current options for the port
+		cfsetispeed(&options, B19200); // Set baud rates
+		cfsetospeed(&options, B19200);
+		options.c_cflag |= (CLOCAL | CREAD); // Enable the receiver and set local mode
+		tcsetattr(fd, TCSANOW, &options); // Set the new options for the port
+		options.c_cflag &= ~CSIZE; // Mask the character size bits
+		options.c_cflag |= CS8; // Select 8 data bits
+		options.c_cflag &= ~PARENB; // No parity
+		options.c_cflag &= ~CSTOPB; // 1 Stop bit
+		options.c_cflag &= ~CSIZE;
+		options.c_cflag |= CS8; // 8 bits
+		options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Raw input mode
+		return fd;
 	}
 
 public:
-	arduinoPortIO(std::string tty, char r = 'r'){
+	arduinoPortIO(std::string tty){
 		this->ttyName = tty;
-		this->fd = open(this->ttyName.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+		this->fd = open_port(ttyName);
 		if(this->fd == -1){
-			throw std::runtime_error("ERR: Could not open " + tty);
+			throw std::runtime_error("ERR: Could not open port " + tty);
 		}
-		this->requestByte = r;
+		tcflush(this->fd, TCIOFLUSH);
 	}
 	~arduinoPortIO(){
 		close(this->fd);
-	}	
+	}
 
+	void writeByte(char c){
+		write(this->fd, &c, 1);
+	}
 
-	/* 
-		* Writes a single byte to the arduino to request data, then waits for the arduino to send the data back.
-		* What this data exactly is is up to the arduino code, but it should be a single integer in this case.
-		* Requesting data instead of the arduino sending data all the time means the values read are more up to date (realtime)
-	*/ 
-	std::string requestDataBufferString(){
-		write(this->fd, &this->requestByte, 1); // Send the request byte
-		usleep(1000); // Wait for the arduino to send the data back
-		// todo: clear /dev/ttyACM(X) buffer, then wait for the arduino to add something to it before reading it
-		this->readIntoBuffer(); // Read the data into the buffer
-		std::string bufferString = ""; // Convert the buffer into an std::string (Makes it easier to work with)
+	void clearBuffer(){
 		for(int i = 0; i < BUFFERSIZE; i++){
+			this->buffer[i] = 0;
+		}
+	}
+
+	int readIntoBuffer(){
+		return read(this->fd, this->buffer, BUFFERSIZE); // Returns number of bytes read
+	}
+
+	std::string getBuffer(){
+		std::string bufferString;
+		for(int i=0; i<BUFFERSIZE; i++){
 			bufferString += this->buffer[i];
 		}
 		return bufferString;
 	}
 	
+	std::string readGetBuffer(){
+		this->readIntoBuffer();
+		return this->getBuffer();
+	}
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,80 @@
+/*
+ * Visualize a graph on which points are added with time.
+ */
+#include <morph/Visual.h>
+#include <morph/ColourMap.h>
+#include <morph/GraphVisual.h>
+#include <morph/Scale.h>
+#include <morph/Vector.h>
+#include <morph/vVector.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <array>
+
 #include "arduinoInterface.hpp"
 
-int main(){
+int main (int argc, char** argv)
+{
     arduinoPortIO arduino("/dev/ttyACM0");
-    while(true){
-        std::cout << arduino.requestDataBufferString();
-        usleep(100000);
+    usleep(1000000);
+
+    int rtn = -1;
+
+    morph::Visual v(1800, 600, "Graph", {0, 0}, {.1,.1,.1}, 2.0f, 0.01f);
+    v.zNear = 0.001;
+    v.backgroundWhite();
+    v.lightingEffects();
+
+    try {
+        morph::GraphVisual<float>* gv = new morph::GraphVisual<float> (v.shaderprog, v.tshaderprog, {-1,-0.5,0});
+
+        // Optionally change the size of the graph and range of the axes
+        gv->setsize (2, 1);
+        // Optionally change the range of the axes
+        int limit = 512;
+        gv->setlimits (-1, limit, -1, 64);
+
+        // Set the graphing policy
+        gv->policy = morph::stylepolicy::lines; // markers, lines, both, allcolour
+        // We 'prepare' a dataset, but won't fill it with data yet. However, we do give the data legend label here.
+        gv->prepdata ("Arduino output", morph::axisside::left);
+
+        gv->ylabel = "Arduino output (CM)";
+        gv->finalize();
+
+        // Add the GraphVisual (as a VisualModel*)
+        v.addVisualModel (static_cast<morph::VisualModel*>(gv));
+
+        v.render();
+        int i = 0;
+        while (v.readyToFinish == false) {
+            glfwWaitEventsTimeout (0.018);
+
+            // Arduino stuff
+            arduino.clearBuffer();
+            std::string data = arduino.readGetBuffer();
+            int dataSTOI;
+            try{
+                dataSTOI = std::stoi(data);
+                gv->append (i, dataSTOI, 0);
+            }catch(...){
+                //std::cout << "STOI error\n";
+            }
+            v.render();
+            i++;
+
+            // Experiment with graphing stuff
+            if(i > limit){
+                i = 0;
+                gv->clear();
+            }
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+        rtn = -1;
     }
-    return 0;
+
+    return rtn;
 }
