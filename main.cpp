7c9d5c2b8163a2d7dd7a72ee0d488f2049f565f8
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 12:02:23 +0100

    Moving average

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main (int argc, char** argv)
     v.lightingEffects();
 
     try {
-        morph::GraphVisual<float>* gv = new morph::GraphVisual<float> (v.shaderprog, v.tshaderprog, {-1,-0.5,0});
+        morph::GraphVisual<float>* gv = new morph::GraphVisual<float> (v.shaderprog, v.tshaderprog, {0,0,0});
 
         // Optionally change the size of the graph and range of the axes
         gv->setsize (2, 1);
@@ -37,30 +37,42 @@ int main (int argc, char** argv)
 
         // Set the graphing policy
         gv->policy = morph::stylepolicy::lines; // markers, lines, both, allcolour
+        gv->axisstyle = morph::axisstyle::twinax;
         // We 'prepare' a dataset, but won't fill it with data yet. However, we do give the data legend label here.
         gv->prepdata ("Arduino output", morph::axisside::left);
+        gv->prepdata ("Arduino output, moving average (last 10 values)", morph::axisside::left);
 
         gv->ylabel = "Arduino output (CM)";
+        gv->ylabel2 = "Moving average (last 10 values)";
         gv->finalize();
 
         // Add the GraphVisual (as a VisualModel*)
         v.addVisualModel (static_cast<morph::VisualModel*>(gv));
+
+        std::vector<int> last10values;
 
         v.render();
         int i = 0;
         while (v.readyToFinish == false) {
             glfwWaitEventsTimeout (0.018);
 
-            // Arduino stuff
+            // == Arduino stuff:
             arduino.clearBuffer();
             std::string data = arduino.readGetBuffer();
             int dataSTOI;
             try{
                 dataSTOI = std::stoi(data);
                 gv->append (i, dataSTOI, 0);
+                last10values.push_back(dataSTOI);
             }catch(...){
                 //std::cout << "STOI error\n";
             }
+            // Moving average
+            if(last10values.size() > 10){
+                last10values.erase(last10values.begin());
+            }
+            gv->append (i, std::accumulate(last10values.begin(), last10values.end(), 0.0) / last10values.size(), 1);
+
             v.render();
             i++;
 
