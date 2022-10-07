83d663ddb504ac9c9271db3a3d99049a38794a01
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 19:11:39 +0100

    Update basicDistanceMeasureSerial.ino

--- basicDistanceMeasureSerial/basicDistanceMeasureSerial.ino
+++ basicDistanceMeasureSerial/basicDistanceMeasureSerial.ino
@@ -35,7 +35,6 @@ void measureDistance(){
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
   
   // Convert the time into a distance
